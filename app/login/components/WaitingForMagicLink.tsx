b51f23513eec89affbcb96a08cf47918abadd6c2
keithinprosper <sharjeelriazsh@gmail.com>
2024-09-29 20:07:40 +0500

    Updated app

--- app/login/components/WaitingForMagicLink.tsx
+++ app/login/components/WaitingForMagicLink.tsx
@@ -25,8 +25,8 @@ export const WaitingForMagicLink = ({
             <Button
               onClick={toggleState}
               className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium 
-            rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 hover:text-white 
-            focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+            rounded-lg border border-transparent bg-orange-600 text-white hover:bg-orange-700 hover:text-white 
+            focus:outline-none focus:bg-orange-700 disabled:opacity-50 disabled:pointer-events-none"
               size="sm"
             >
               <ArrowLeft size={14} />
